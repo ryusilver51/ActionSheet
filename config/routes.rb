2d611e4dbaaef1bcb31a949e65329c2b5ff46b3a
ryunosuke <ryunosuke3581@gmail.com>
2020-02-25 23:37:19 +0900

    Add card_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
-  resources :lists, only: [:new, :create, :edit, :update, :destroy]
+  resources :lists, only: [:new, :create, :edit, :update, :destroy] do
+    resources :cards, only: [:new, :create]
+  end
 end
