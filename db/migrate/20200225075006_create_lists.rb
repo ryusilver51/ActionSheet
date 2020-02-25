b7c0e7192b88650f2e099eb5e69eaca755296868
ryunosuke <ryunosuke3581@gmail.com>
2020-02-25 22:59:22 +0900

    Create Card_model

--- db/migrate/20200225075006_create_lists.rb
+++ db/migrate/20200225075006_create_lists.rb
@@ -1,7 +1,6 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
-    create_table :lists do |t|
-      
+    create_table :lists do |t|      
       t.string :title, null: false
       t.references :user, foreign_key: true, null: false
       t.timestamps
